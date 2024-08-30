34bed1222f1319579e45b6e819e97c5f7e8d674f
AnnaKarolineNunes <annamiranda894@gmail.com>
2024-08-30 17:15:37 -0300

    finalização do front end

--- frontend/react-cadastro/src/App.jsx
+++ frontend/react-cadastro/src/App.jsx
@@ -1,13 +1,22 @@
+import {BrowserRouter, Route, Routes, Navigate} from "react-router-dom";
+import Cadastro from "./pages/Cadastro";
+import Login from "./pages/Login";
+import ListarUsuarios from "./pages/Lista";
 
 function App() {
   
 
   return (
-    
-      <div>
-        <h1> Cadastro </h1>
-      </div>
-     
+      <BrowserRouter>
+      <header className="bg-blue-600 text-white p-4 shadow-md">
+        <h1 className="text-2xl font-bold text-center">Sistema de Cadastro de Usuários</h1>
+      </header>
+        <Routes>
+          <Route path="/" element={<Cadastro />} />
+          <Route path="/Login" element={<Login />} />
+          <Route path="/listar-usuarios" element={<ListarUsuarios />} />
+        </Routes>
+      </BrowserRouter>
   )
 }
 
